7ba1e6b0af3f876f12510ff63d2939898b8c452b
Dylan Friedman <FRDDYL002@myuct.ac.za>
2024-06-15 09:48:13 +0200

    First full iteration done

--- src/app/_components/Hero/hero.tsx
+++ src/app/_components/Hero/hero.tsx
@@ -1,20 +1,28 @@
+// Import necessary components and modules from their respective libraries
 import { Button, Link } from "@nextui-org/react";
 import Container from "$/src/app/_components/Container/container";
 import { SignedOut } from "@clerk/nextjs";
 import { siteConfig } from "$/src/config/site";
 
+// Define a functional component named Hero
 const Hero = () => {
+  // The component returns a Container component with certain styling
   return (
     <Container className="flex h-screen flex-col items-center text-center">
+      {/* A div to contain the main content with responsive width settings */}
       <div className="w-9/12 md:w-6/12">
+        {/* A heading with responsive text sizes */}
         <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
           Meow
         </h1>
+        {/* A paragraph with responsive text sizes and custom styles */}
         <p className="text-body-color dark:text-body-color-dark mb-12 text-base !leading-relaxed sm:text-lg md:text-xl">
           Accept support through Open Payments. Start a campaign. Setup a
           campaign monetary goal. It is easier than you think.
         </p>
+        {/* A div to arrange buttons with specific styling */}
         <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+          {/* Only render the Sign Up button if the user is signed out */}
           <SignedOut>
             <Button
               className="rounded-sm px-8 py-4 text-base font-semibold"
@@ -24,6 +32,7 @@ const Hero = () => {
               Sign Up
             </Button>
           </SignedOut>
+          {/* A secondary button linking to an external site */}
           <Button
             className="rounded-sm px-8 py-4 text-base font-semibold"
             size="lg"
@@ -43,4 +52,5 @@ const Hero = () => {
   );
 };
 
+// Export the Hero component as the default export
 export default Hero;
--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 
+
+
+
 import Container from "../_components/Container/container";
 import {
-  Button,
-  CardBody,
-  Input,
-  Image,
-  useDisclosure,
+Button,
+CardBody,
+Input,
+Image,
+useDisclosure,
 } from "@nextui-org/react";
 import { useUser } from "@clerk/nextjs";
 import { Card } from "@nextui-org/react";
@@ -16,25 +19,104 @@ import { useFormState } from "react-dom";
 import { api } from "$/src/trpc/react";
 import UserCard from "../_components/User/user";
 
+
+
+
 export default function Campaign({ params }: { params: { id: string } }) {
-  const { user } = useUser();
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+const { user } = useUser();
+const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  const campaign = api.campaigns.getOne.useQuery({
-    id: params.id ?? "",
-  });
 
-  const users = api.users.get.useQuery({});
 
-  return (
-    <Container className="flex flex-col items-center">
-      {users.data && (
-        <div className="grid w-full grid-cols-1 gap-2 overflow-scroll p-4 md:grid-cols-3 xl:grid-cols-4">
-          {users.data.data.map((user) => (
-            <UserCard key={user.id} user={user}></UserCard>
-          ))}
-        </div>
-      )}
-    </Container>
-  );
+
+const campaign = api.campaigns.getOne.useQuery({
+  id: params.id ?? "",
+});
+
+
+
+
+const users = api.users.get.useQuery({});
+
+
+
+
+const ids = users.data?.data.map(user => user.id) ?? [];
+const userIds = users.data?.data.map(user => user.userId) ?? [];
+
+
+
+
+
+
+
+
+ const usersHardcode1 = [
+  {
+  
+    id: ids[0],
+    userId: userIds[0],
+    email: "FinancialAid@uct.ac.za",
+    firstName: "University of Cape Town",
+    about: "UCT is a community of exceptionally talented students, teachers and researchers – and a wide range of professional, administrative support and service staff – all of whom are committed to help change this world for the better. We encourage one another to work hard, not only to earn degrees or public recognition, but also to be leaders in this increasingly changing world.",
+    instagram: "john.doe",
+    twitter: "johndoe",
+    linkedin: "johndoe",
+    imageUrl: "https://www.lstmed.ac.uk/sites/default/files/styles/mc-580-16x9-node/public/content/pages/images/UCT.jpeg?itok=O0dc31th"
+  },
+];
+
+
+
+
+const usersHardcode2 = [
+  {
+    id: "124",
+    userId: "123457",
+    email: "nsfas@sun.ac.za",
+    firstName: "Stellenbosch University",
+    about: "Stellenbosch University (SU) is a public research university situated in Stellenbosch, a town in the Western Cape province of South Africa.",
+    instagram: "john.doe",
+    twitter: "johndoe",
+    linkedin: "johndoe",
+    imageUrl: "https://yiba.co.za/wp-content/uploads/2021/04/Stellenbosch_University_new_logo_for-web-01.jpg"
+  },
+];
+
+
+
+
+return (
+  <>
+
+
+
+
+  <Container className="flex flex-col items-center">
+    {users.data && (
+      <div className="grid w-full grid-cols-1 gap-2 overflow-scroll p-4 md:grid-cols-3 xl:grid-cols-4">
+        {usersHardcode1.map((user) => (
+          <UserCard key={user.id} user={user}></UserCard>
+        ))}
+      </div>
+    )}
+  </Container>
+
+
+
+
+  <Container className="flex flex-col items-center">
+    {users.data && (
+      <div className="grid w-full grid-cols-1 gap-2 overflow-scroll p-4 md:grid-cols-3 xl:grid-cols-4">
+        {usersHardcode2.map((user) => (
+          <UserCard key={user.id} user={user}></UserCard>
+        ))}
+      </div>
+    )}
+  </Container>
+   </>
+);
 }
+
+
+
